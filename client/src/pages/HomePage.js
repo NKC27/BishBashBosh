b0f427d94b1f2ede5586d9fcd8e275f2f5b3b617
roconn <roconn25@gmail.com>
2022-06-11 20:11:36 +0100

    added homepage + menubar

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuBar from "../components/MenuBar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <MenuBar />
+    </div>
+  );
+};
+
+export default HomePage;
